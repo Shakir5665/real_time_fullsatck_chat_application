2f962815fc204f7a168095358c48747721ae0e6c
Mohamed shakir <shakirsar225974@gmail.com>
2025-08-13 13:23:34 +0530

    chat Area Update

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils';
 
 const ChatContainer = ({selectedUser,setSelectedUser}) => {
+
+
+  const scrollEnd = useRef();
+
+  useEffect(() => {
+
+    if(scrollEnd.current){
+       scrollEnd.current.scrollIntoView({behavior:"smooth"})
+    }
+
+  },[])
+
+
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
        
@@ -18,13 +32,45 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
       </div>
 
       {/*------------------------ Chat Area ----------------------------- */}
-      <div classsName ='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+      <div className ='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
           {messagesDummyData.map((msg,index) => (
-            <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse' }`}>
+            <div key={index} className = {`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse' }`}>
+              {
+                msg.image ? (
+                  <img src={msg.image} className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8' />
+                ) : (
+                  <p className = {`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'} `} >{msg.text}</p>
+                )
+              }
+              <div className='text-center text-xs'>
+                  <img src ={msg.senderId === "680f50e4f10f3cd28382ecf9" ? assets.avatar_icon :assets.profile_martin} className='w-7 rounded-full' />
+                  <p className='text-gray-500'> {formatMessageTime(msg.createdAt)} </p>
+              </div>
               
 
             </div>
           ))}
+
+          <div ref={scrollEnd}>
+
+          </div>
+
+      </div>
+
+      {/*------------------------------------------- Bottom Area -------------------------------------------------- */}
+      <div className='bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+          <div className='flex flex-1 items-center bg-gray-100/12 px-3 rounded-full'>
+             <input type="text" placeholder='Send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+             <input type="file" id='image' accept='image/png,image-jpg' hidden />
+             <label htmlFor="image">
+                <img src={assets.gallery_icon} className='w-5 mr-2 cursor-pointer' />
+             </label>
+          </div>
+          <div>
+            <img src={assets.send_button} className='w-7 cursor-pointer' />
+
+          </div>
+
       </div>
 
     </div>
@@ -37,3 +83,4 @@ const ChatContainer = ({selectedUser,setSelectedUser}) => {
 }
 
 export default ChatContainer
+ 
