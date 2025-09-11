0e6f54249c0d36c0356a024216338af92e41b5cb
Mohamed shakir <shakirsar225974@gmail.com>
2025-09-11 18:12:35 +0530

    Renamed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
 
         <Route path="/" element={authUser? <HomePage/> : <Navigate to = "/login"/>} />
-        <Route path="/login" element={!authUser? <Loginpage/> :<Navigate to = "/"/> } />
+        <Route path="/login" element={!authUser? <LoginPage/> :<Navigate to = "/"/> } />
         <Route path="/profile" element={authUser? <ProfilePage/> : <Navigate to = "/login"/>} />
 
       </Routes>
